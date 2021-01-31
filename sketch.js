8d56c98e05710fa0151d8f9f1d59dd456a27645c
PWhitehat <77223407+PWhitehat@users.noreply.github.com>
2021-01-31 16:37:08 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,17 +1,30 @@
+// variables
 var fixedrect, movingrect;
+var test1, test2;
 
 function setup() {
 
+  // creates canvas
   createCanvas(1200,800);
 
+  //makes 2 rectangles
   fixedrect = createSprite(200, 200, 50, 80);
   movingrect = createSprite(400, 200, 80, 50);
 
-  fixedrect.debug = true;
-  movingrect.debug = true;
+  //fixedrect.debug = true;
+  //movingrect.debug = true;
 
   fixedrect.shapeColor = "green";
   movingrect.shapeColor = "blue";
+
+  test1 = createSprite(300, 300, 50, 20);
+  test2 = createSprite(600, 300, 60, 20);
+
+  test1.shapeColor = "yellow";
+  test2.shapeColor = "cyan";
+
+  test1.velocityX = 3;
+  test2.velocityX = -3;
 
 }
 
@@ -22,21 +35,21 @@ function draw() {
   movingrect.x = World.mouseX;
   movingrect.y = World.mouseY;
 
-  if ((movingrect.x - fixedrect.x) < (movingrect.width / 2 + fixedrect.width / 2) 
-  && (fixedrect.x - movingrect.x) < (movingrect.width / 2 + fixedrect.width / 2)
-  && (movingrect.y - fixedrect.y) < (movingrect.height / 2 + fixedrect.height / 2) 
-  && (fixedrect.y - movingrect.y) < (movingrect.height / 2 + fixedrect.height / 2)) {
+  if (isTouching(movingrect, fixedrect)) {
 
     movingrect.shapeColor = "red";
     fixedrect.shapeColor = "red";
 
   }
+
   else {
 
-    movingrect.shapeColor = "blue";
     fixedrect.shapeColor = "green";
+    movingrect.shapeColor = "blue";
 
   }
+
+  bounceOff(test1, test2);
 
   //console.log(movingrect.x - fixedrect.x);
 
